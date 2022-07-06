5ff44dc8f0dafccb970133e11cca1871651a2df6
Hendra Battousai <71735045+HendraBattousai@users.noreply.github.com>
2022-07-06 22:28:00 +0700

    Add files via upload

---
+++ lib/style.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color colorPrimary = Color.fromARGB(255, 25, 45, 82);
+Color orangeColor = Color.fromARGB(255, 245, 66, 16);
+Color blackColor = Color.fromARGB(255, 0, 0, 0);
+Color whiteColor = Color.fromARGB(255, 255, 255, 255);
+Color greyColor = Color.fromARGB(255, 80, 80, 80);
+Color greenColor = Color.fromARGB(255, 0, 129, 11);
+
+const double edge = 24;
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: blackColor,
+);
+
+TextStyle whiteTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: whiteColor,
+);
+
+TextStyle greyTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w300,
+  color: greyColor,
+);
+
+TextStyle primaryTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w500,
+  color: colorPrimary,
+);
+
+TextStyle regularTextStyle = GoogleFonts.poppins(
+  fontWeight: FontWeight.w400,
+  color: colorPrimary,
+);
